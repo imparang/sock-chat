fbc00ac14dbb955b4781983a08e7d7f93bf0de76
imparang <jeongjaeyn@gmail.com>
2021-10-07 16:51:34 +0900

    feat: Add clubsRouter
    route '/clubs'
    route '/clubs/:filename'

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const http = require('http');
 const server = http.createServer(app);
 
 const indexRouter = require('./routes/index');
+const clubsRouter = require('./routes/clubs');
 
 // main이 아닌 -> indexRouter로 들어가도록 한다.
 app.use('/', indexRouter);
+app.use('/clubs', clubsRouter);
 
 server.listen(3000, () => {
   console.log('Express Server is now listening on localhost: 3000..');
